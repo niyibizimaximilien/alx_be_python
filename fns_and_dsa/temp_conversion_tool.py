21995139d0c9ac734fd39367f308ad869c8004a6
maximilien <niyibizimaximilien@gmail.com>
2025-07-22 17:29:43 +0200

    .

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,8 @@
+# File: fns_and_dsa/temp_conversion_tool.py
+
+# Global conversion factors
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32)
