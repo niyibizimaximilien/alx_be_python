2ffc5e22a6ab3bcf12ecdf21a4621387f002222a
NIYIBIZI Maximilien <2kapomaxime@gmail.com>
2025-07-25 16:32:28 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,7 +6,7 @@ def perform_operation(num1, num2, operation):
     elif operation == 'subtract':
         return num1 - num2
     elif operation == 'multiply':
-        return num1 * num
+        return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
             return "Error: Cannot divide by zero"
